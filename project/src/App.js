1143f5257b15ab679b4edcc3d0438ed39049fd64
shaikshabbeercivil145 <153210204+shaikshabbeercivil145@users.noreply.github.com>
2024-03-24 17:44:43 +0000

    User component added

---
+++ project/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return <div>
+      <h1>Users: </h1>
+
+   < section className="container">
+    <User  name="shabeena" job="ElectricalEngineering" location="Vallur"/> 
+    <User name="sameer" job="MechatronicsEngineering" location="Delhi"/>
+    <User name="mahabasha" job="PetroleumEngineering" location="Chennai"/>
+
+    </section>
+  </div>
+}
+
+function User(props){
+  return<div className="user">
+    <h1>{props.name}</h1>
+    <p>Job: {props.job}</p>
+    <p>Location: {props.location}</p>
+  </div>
+}
+
+export default App;
